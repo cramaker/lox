bdb2457c7921c9c6ef588f4f1c32765fcab5605d
Cody Ramaker <cramaker@linux.com>
2020-03-12 17:03:35 -0700

    Wiring up the parser

--- src/main/java/lox/Lox.java
+++ src/main/java/lox/Lox.java
@@ -49,10 +49,13 @@ public class Lox {
     public static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
 
-        // For now, just print the tokens.
-        for (Token token: tokens) {
-            System.out.println(token);
+        // Stop if there was a syntax error
+        if (hadError) return;
+
+        System.out.println(new AstPrinter().print(expression));
         }
     }
 
