90524802416f7b4ae8ec04b16fa109737d3f5646
Cody Ramaker <cramaker@linux.com>
2020-03-21 17:52:05 -0700

    Added check for unary truthiness

--- Interpreter.java
+++ Interpreter.java
@@ -11,12 +11,20 @@ class Interpreter implements Expr.Visitor<Object> {
         Object right = evaluate(expr.right);
 
         switch (expr.operator.type) {
+        case BANG:
+            return !isTruthy(right);
         case MINUS:
             return -(double)right;
         }
 
         // Unreachable
         return null;
+    }
+
+    private boolean isTruthy(Object object) {
+        if (object == null) return false;
+        if (object instanceOf Boolean) return (boolean)object;
+        return true;
     }
 
     @Override
