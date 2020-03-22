20548a02cce426fb4e0c77bcbef584058695c523
Cody Ramaker <cramaker@linux.com>
2020-03-22 11:29:39 -0700

    Added logic for evaluating binary operators

--- Interpreter.java
+++ Interpreter.java
@@ -27,6 +27,14 @@ class Interpreter implements Expr.Visitor<Object> {
         return true;
     }
 
+    private boolean isEqual(Object a, Object b) {
+        // nil is only equal to nil
+        if (a == null && b == null) return true;
+        if (a == null) return false;
+
+        return a.equals(b);
+    }
+
     @Override
     public Object visitGroupingExpr(Expr.Grouping expr) {
         return evaluate(expr.expression);
@@ -34,5 +42,41 @@ class Interpreter implements Expr.Visitor<Object> {
 
     private Object evaluate(Expr expr) {
         return expr.accept(this);
+    }
+
+    @Ovverride
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+        case GREATER:
+            return (double)left > (double)right;
+        case GREATER_EQUAL:
+            return (double)left >= (double)right;
+        case LESS:
+            return (double)left < (double)right;
+        case LESS_EQUAL:
+            return (double)left <= (double)right;
+        case BANG_EQUAL: return !isEqual(left, right);
+        case EQUAL_EQUAL: return isEqual(left, right);
+        case MINUS:
+            return (double)left - (double)right;
+        case PLUS:
+            if (left instanceof Double && right instanceof Double) {
+                return (double)left + (double)right;
+            }
+
+            if (left instanceof String && right instanceof String) {
+                return (String)left + (String)right;
+            }
+        case SLASH:
+            return (double)left / (double)right;
+        case STAR:
+            return (double)left * (double)right;
+        }
+
+        // Unreachable
+        return null;
     }
 }
