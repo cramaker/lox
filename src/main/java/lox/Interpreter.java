7fb2bd631ca89a31c087c0259bd8ba0f649ba9be
Cody Ramaker <cramaker@linux.com>
2020-03-25 17:58:15 -0700

    Runtime error for + if neither string nor number

--- src/main/java/lox/Interpreter.java
+++ src/main/java/lox/Interpreter.java
@@ -87,6 +87,8 @@ class Interpreter implements Expr.Visitor<Object> {
             if (left instanceof String && right instanceof String) {
                 return (String)left + (String)right;
             }
+
+            throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings.");
         case SLASH:
             checkNumberOperands(expr.operator, left, right);
             return (double)left / (double)right;
